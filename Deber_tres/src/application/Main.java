6dd3fdbec1cf3ee34814db02df4e2b156d49abe8
vnssmb <142614155+vnssmb@users.noreply.github.com>
2024-06-12 00:40:39 -0500

    Add files via upload

---
+++ Deber_tres/src/application/Main.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Control.fxml"));
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setScene(new Scene(root, 331, 574));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
